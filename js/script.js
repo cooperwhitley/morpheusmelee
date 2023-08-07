fa0fbffa84757696bc56391ac49d5f6d1d92b655
cooperwhitley <whitleycooperr@gmail.com>
2023-08-06 21:27:20 -0500

    added text boxes and a variable to store them in

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 /*---constants---*/
-
+// store menu option divs in an array
+const menuOptionDivs = [];
+for (i = 0; i <= 3; i++) {
+    menuOptionDivs.push(document.getElementById(`option-${i}`));
+}
+menuOptionDivs.forEach((item) => item.innerText = 'sample');
 
 
 /*---state variables---*/
