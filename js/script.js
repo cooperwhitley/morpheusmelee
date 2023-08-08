d0045f959c79726f0d54005c82518826bf8260ed
Cooper Whitley <whitleycooperr@gmail.com>
2023-08-08 14:03:07 -0500

    objectifying chars, game logic

--- js/script.js
+++ js/script.js
@@ -1,59 +1,84 @@
 /*---constants---*/
 // player attacks
-const attacks = [
-    { 
-        name: 'BITE', 
-        info: 'MORPHEUS BIT', 
-        pawLocation: '5 / 1 / 6 / 2',
-        dmg: 10
-    },
-    { 
-        name: 'SCRATCH',
-        info: 'MORPHEUS SCRATCHED THE SHIT OUT OF',
-        pawLocation: '6 / 1 / 7 / 2',
-        dmg: 10
-    },
-    {
-        name: 'LUNGE',
-        info: 'MORPHEUS LUNGED AT',
-        pawLocation: '5 / 6 / 6 / 7',
-        dmg: 15
-    },
-    {
-        name: 'SWIPE',
-        info: 'MORPHEUS SWIPED AT',
-        pawLocation: '6 / 6 / 7 / 7',
-        dmg: 5
-    }
-];
+const player = {
+    name: 'MORPHEUS',
+    attacks: [
+        { 
+            name: 'BITE', 
+            info: 'MORPHEUS BIT', 
+            pawLocation: '5 / 1 / 6 / 2',
+            dmg: 10
+        },
+        { 
+            name: 'SCRATCH',
+            info: 'MORPHEUS SCRATCHED THE SHIT OUT OF',
+            pawLocation: '6 / 1 / 7 / 2',
+            dmg: 10
+        },
+        {
+            name: 'LUNGE',
+            info: 'MORPHEUS LUNGED AT',
+            pawLocation: '5 / 6 / 6 / 7',
+            dmg: 15
+        },
+        {
+            name: 'SWIPE',
+            info: 'MORPHEUS SWIPED AT',
+            pawLocation: '6 / 6 / 7 / 7',
+            dmg: 5
+        }
+    ],
+    reactions: [
+        'MEOW', 
+        'MROW', 
+        'MROWOW', 
+        'MEOWWWWWWW'
+    ]
+};
 const menuOptionLocations = [
     ['top-left', 'top-right'],
     ['bottom-left', 'bottom-right']
 ];
 // enemy attacks
     // cooper attacks
-const cooperAttacks = [
-    {
-        name: 'NERF GUN',
-        info: 'COOPER SHOT AT MORPHEUS',
-        dmg: -10
-    },
-    {
-        name: 'PET',
-        info: 'COOPER PET MORPHEUS AND SCRATCHED HIM BEHIND THE EAR',
-        dmg: -5
-    },
-    {
-        name: 'GIVE TREAT',
-        info: "COOPER GAVE MORPHEUS A TASTY TREAT, HE DOESN'T DO THAT ENOUGH",
-        dmg: -20
-    },
-    {
-        name: 'SHOUT',
-        info: 'COOPER YELLED AT MORPHEUS: "QUIT BEING AN ASSHOLE"',
-        dmg: -5
-    }
-]
+const cooper = {
+    name: 'COOPER',
+    attacks: [
+        {
+            name: 'NERF GUN',
+            info: 'COOPER SHOT AT MORPHEUS',
+            dmg: -10
+        },
+        {
+            name: 'PET',
+            info: 'COOPER PET MORPHEUS AND SCRATCHED HIM BEHIND THE EAR',
+            dmg: -5
+        },
+        {
+            name: 'GIVE TREAT',
+            info: "COOPER GAVE MORPHEUS A TASTY TREAT, HE DOESN'T DO THAT ENOUGH",
+            dmg: -20
+        },
+        {
+            name: 'SHOUT',
+            info: 'COOPER YELLED AT MORPHEUS: "QUIT BEING AN ASSHOLE"',
+            dmg: -5
+        }
+    ],
+    greetings: [
+        'HI SWEET BOY', 
+        'WHATCHA DOIN MORBY', 
+        'AWW WASUP ANGEL BOY', 
+        'MORBY!'
+    ],
+    reactions: [
+        'WHY', 
+        'OUCH!', 
+        'HEY! WHAT WAS THAT ABOUT', 
+        'WHY ARE YOU LIKE THIS'
+    ],
+    health: 0
+}
 
 /*---state variables---*/
 
@@ -69,6 +94,16 @@ let morphBoyMode = 1;
 let textPrinted = false;
 // store damage to be applied
 let damageToApply;
+// move choice
+let moveChoice;
+
+// enemy choice
+let enemy = cooper;
+
+// health
+enemy.health = 100;
+player.health = 100;
+
 
 /*---cached elements---*/
 
@@ -94,54 +129,85 @@ const gameWindow = document.getElementById('game-window');
 const bodyEl = document.querySelector('body');
 /*---functions---*/
 
-// init
+// init();
 function init() {
     winner = null;
     turn = 0;
     menu = 0;
+    turn0();
 }
-
 // turn logic
 
 // turn 0 - enemy greets morpheus
-function turnZero() {
+function turn0() {
     // make textbox visible and menu choices invisible
+    textBoxEl.style.visibility = 'visible'
+    for (let option of menuOptionEls) {
+        option.style.visibility = 'hidden'
+    }
     // print greeting text
+    let randomGreeting = `${enemy.name}: ${enemy.greetings[Math.floor(Math.random() * enemy.greetings.length)]}`
+    typeSentence(randomGreeting, textBoxEl)
     // move pawprint to bottom right of menu and only show if textPrinted is true
+    waitAndRenderSelectorEl(randomGreeting);
 }
 
 // turn 1 - morpheus chooses attack
-function turnOne() {
+function turn1() {
     // make textbox invisible and menu choices visible
+    textBoxEl.style.visibility = 'hidden'
+    for (let option of menuOptionEls) {
+        option.style.visibility = 'visible'
+    }
     // move pawprint based on highlighted menu selection
+    // deploy action titles to menu option divs and add hover icon
+    menuOptionEls.forEach((option) => {
+        option.innerText = player.attacks[menuOptionEls.indexOf(option)].name;
+        option.addEventListener('mouseover', moveSelectorElByHover);
+        option.addEventListener('mouseout', removeSelectorElAfterHover);
+        menuSelectorEl.style.justifySelf = 'flex-end';
+    });
     // listen for action choice
 }
 
+moveChoice = player.attacks[1]
+damageToApply = player.attacks[1].dmg
 // turn 2 - attack animation and flavor text for morpheus' attack
-function turnTwo() {
+function turn2() {
     // make textPrinted false
+    textPrinted = false;
     // make textbox visible and menu choices invisible
+    textBoxEl.style.visibility = 'visible'
+    for (let option of menuOptionEls) {
+        option.style.visibility = 'hidden'
+    }
     // print action text
+    let actionText = `${moveChoice.info} ${enemy.name}`;
+    console.log(actionText)
+    typeSentence(actionText, textBoxEl);
+    waitAndRenderSelectorEl(actionText);
     // apply and animate damage received
-    // move pawprint to bottom right of menu
+    enemy.health += damageToApply;
+    enemyHealth.value += damageToApply;
     // check for winner
 }
+turn2();
 
 // turn 3 - enemy reaction text to morpheus' attack
-function turnThree() {
+function turn3() {
     // make textPrinted false
     // if winner = true print win message, else print reaction text
 }
 
 // turn 4 - enemy chooses attack
-function turnFour() {
+function turn4() {
     // make textPrinted false
     // choose random enemy attack
     // print what attack is chosen
 }
 
 // turn 5 - enemy attack animation & text
-function turnFive() {
+function turn5() {
     // make textPrinted false
     // print action text
     // apply and animate damage received
@@ -149,9 +215,19 @@ function turnFive() {
 }
 
 // turn 6 - morpheus reaction text to enemy attack
-function turnSix() {
+function turn6() {
     // make textPrinted false
     // if winner = true print win message, else print reaction text
+}
+
+function advanceTurn() {
+    // check if textPrinted
+    // advance to next turn
+}
+
+function handleMoveChoice() {
+    // set move choice
+    // set damage to apply
 }
 
 // /turn logic
@@ -162,7 +238,7 @@ function turnSix() {
 // move indicator based on hover
 function moveSelectorElByHover(event) {
     menuSelectorEl.style.visibility = 'visible';
-    menuSelectorEl.style.gridArea = attacks[menuOptionEls.indexOf(event.target)].pawLocation;
+    menuSelectorEl.style.gridArea = player.attacks[menuOptionEls.indexOf(event.target)].pawLocation;
 }
 function removeSelectorElAfterHover() {
     menuSelectorEl.style.visibility = 'hidden';
@@ -179,14 +255,27 @@ function toggleMorphBoyMode() {
     }
 };
 
+function pain(target) {
+    target.style.animation = 'pain 50ms linear 5'
+}
+
+// 
 // text print fn
+async function waitAndRenderSelectorEl(str) {
+    const timeToWait = (str.length + 1) * 50;
+    await waitForMs(timeToWait);
+    textPrinted = true;
+    menuSelectorEl.style.visibility = 'visible';
+    menuSelectorEl.style.gridArea = '6 / 10 / 7 / 11';
+    menuSelectorEl.style.justifySelf = 'center';
+}
+
 async function typeSentence(str, elId) {
     const letters = str.split('');
     let i = 0;
     let typingTarget = elId;
-    // time to wait calculates how long to wait for textPrinted to be true
     while (i < letters.length) {
-        await waitForMs(100);
+        await waitForMs(50);
         typingTarget.append(letters[i]);
         i++
     }
@@ -210,14 +299,11 @@ function waitForMs(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+
+
 /*---event listeners---*/
 
-// deploy action titles to menu option divs and add hover icon
-menuOptionEls.forEach((option) => {
-    option.innerText = attacks[menuOptionEls.indexOf(option)].name;
-    option.addEventListener('mouseover', moveSelectorElByHover);
-    option.addEventListener('mouseout', removeSelectorElAfterHover);
-});
+
     // menu choices
 // menu hover
 // textbox advance
